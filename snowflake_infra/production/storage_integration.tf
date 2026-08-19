d61d409d7a2ef86903007b1d6190ebec574034b0
Jhoie <victoriousjvictor@gmail.com>
2026-08-19 19:19:43 +0100

    Stage and file format

--- snowflake_infra/production/storage_integration.tf
+++ snowflake_infra/production/storage_integration.tf
@@ -6,3 +6,18 @@ resource "snowflake_storage_integration_aws" "storage_integration" {
   storage_allowed_locations = ["s3://solaz-supplychain-lake/telemetry/"]
   storage_aws_role_arn      = "arn:aws:iam::049417293525:role/elite-snowflake-role"
 }
+
+resource "snowflake_stage_external_s3" "stage" {
+  name                = "BRONZE_STAGE"
+  database            = "PROD_DB"
+  schema              = "BRONZE"
+  url                 = "s3://solaz-supplychain-lake/telemetry/"
+  storage_integration = snowflake_storage_integration_aws.storage_integration.name
+}
+
+resource "snowflake_file_format" "bronze_json_format" {
+  name        = "BRONZE_JSON_FORMAT"
+  database    = "PROD_DB"
+  schema      = "BRONZE"
+  format_type = "JSON"
+}
