8faa94be879da62fdd440a64c3e1628065c478a0
Jhoie <victoriousjvictor@gmail.com>
2026-08-19 11:05:07 +0100

    Storage integration

---
+++ snowflake_infra/production/storage_integration.tf
@@ -0,0 +1,8 @@
+resource "snowflake_storage_integration_aws" "storage_integration" {
+  name             = "SOLAZ_PROD_S3_STORAGE_INTEGRATION"
+  enabled          = true
+  storage_provider = "S3"
+
+  storage_allowed_locations = ["s3://solaz-supplychain-lake/telemetry/"]
+  storage_aws_role_arn      = "arn:aws:iam::049417293525:role/elite-snowflake-role"
+}
