4ed01cb92f90b72388d76cfc026eeadf3bf03635
Jhoie <victoriousjvictor@gmail.com>
2026-09-01 20:27:12 +0100

    Fix environment credential_id

--- dbt_cloud_infra/environment.tf
+++ dbt_cloud_infra/environment.tf
@@ -5,7 +5,7 @@ resource "dbtcloud_environment" "prod" {
   deployment_type = "production"
 
   connection_id = dbtcloud_global_connection.dbt_cloud_snowflake_connection.id
-  credential_id = dbtcloud_snowflake_credential.dbt_cloud_snowflake_credentials.id
+  credential_id = dbtcloud_snowflake_credential.dbt_cloud_snowflake_credentials.credential_id
 
   dbt_version = var.dbt_version
 }
@@ -17,7 +17,7 @@ resource "dbtcloud_environment" "slim_ci" {
   deployment_type = "staging"
 
   connection_id = dbtcloud_global_connection.dbt_cloud_snowflake_connection.id
-  credential_id = dbtcloud_snowflake_credential.slim_ci_snowflake_credentials.id
+  credential_id = dbtcloud_snowflake_credential.slim_ci_snowflake_credentials.credential_id
 
   dbt_version = var.dbt_version
 }
